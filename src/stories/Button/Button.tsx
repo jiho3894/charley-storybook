c5622344b5d5ad33203c90c559cb334e01a1cac2
김지호 <crsn1111@gmail.com>
2023-07-15 19:51:13 +0900

    feat: onclick setting

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 export interface IButtonProps {
   text: string;
+  onClick: () => void;
 }
 
-const Button = ({ text = '제출하기' }: IButtonProps) => {
+const Button = ({ text = '제출하기', onClick }: IButtonProps) => {
   return (
-    <div className="w-[200px] h-[40px] bg-[#5d04d9] rounded-[10px] m-auto flex justify-center items-center">
+    <div
+      onClick={onClick}
+      className="w-[200px] h-[40px] bg-[#5d04d9] rounded-[10px] m-auto flex justify-center items-center"
+    >
       <span className="text-white">{text}</span>
     </div>
   );
