136d2423e44f21576b1a5a45e1285b26214020cd
김지호 <crsn1111@gmail.com>
2023-07-16 16:18:58 +0900

    feat: 복구

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/stories/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/stories/**/*.{js,jsx,ts,tsx}'],
   darkMode: false,
   theme: {
     extend: {},
