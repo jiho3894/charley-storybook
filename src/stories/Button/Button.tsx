36e7edd78c759e6c8bad80a048c5bb0dcdc92b21
김지호 <crsn1111@gmail.com>
2023-07-16 16:26:27 +0900

    feat: change base -> extend

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -7,7 +7,10 @@ export interface IButtonProps {
 
 const Button = ({ text = '제출하기', onClick }: IButtonProps) => {
   return (
-    <div onClick={onClick} className="button">
+    <div
+      onClick={onClick}
+      className="w-[200px] h-[40px] bg-charley rounded-[10px] m-auto flex justify-center items-center"
+    >
       <span className="text-white">{text}</span>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/stories/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        charley: '#5d04d9',
+      },
+    },
   },
   variants: {},
   plugins: [],
